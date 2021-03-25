e65c3b0dee56ef5aa6d0861959fe6e7576707114
菘菘 <rrt467778@gmail.com>
2021-03-25 18:36:03 +0800

    將程式碼變得更短w

--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/PackFinder.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/PackFinder.java
@@ -1,166 +1,20 @@
 package siongsng.rpmtwupdatemod;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
-
-import com.google.common.base.Supplier;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-
 import net.minecraft.resources.FilePack;
-import net.minecraft.resources.FolderPack;
 import net.minecraft.resources.IPackFinder;
 import net.minecraft.resources.IPackNameDecorator;
-import net.minecraft.resources.IResourcePack;
 import net.minecraft.resources.ResourcePackInfo;
-import net.minecraft.resources.ResourcePackInfo.IFactory;
-import net.minecraft.resources.ResourcePackInfo.Priority;
-import net.minecraft.resources.data.IMetadataSectionSerializer;
-import net.minecraft.util.JSONUtils;
 
-public final class PackFinder implements IPackFinder {
+import java.util.function.Consumer;
 
-    private static final IMetadataSectionSerializer<PackMeta> META_SERIALIZER = new PackMetaSerializer();
-    public static final PackFinder RESOUCE = new PackFinder(Type.RESOURCES);
-
-    public static class Configuration {
-        public static boolean allowDataPacks() {
-            return true;
-        }
-    }
-
-    private final Type type;
-    private final File loaderDirectory;
-
-    private PackFinder(Type type) {
-        this.type = type;
-        this.loaderDirectory = new File(type.path);
-        try {
-            Files.createDirectories(this.loaderDirectory.toPath());
-        } catch (final IOException e) {
-            RpmtwUpdateMod.LOGGER.error("初始化載入器失敗", e);
-        }
-    }
-
+public class PackFinder implements IPackFinder {
     @Override
-    public void findPacks(Consumer<ResourcePackInfo> packs, IFactory factory) {
-        if (!this.type.enabled.getAsBoolean()) {
-            RpmtwUpdateMod.LOGGER.info("{} 透過config禁止載入", this.type.displayName);
-            return;
-        }
-
-        for (final File packCandidate : getFilesFromDir(this.loaderDirectory)) {
-            final boolean isFilePack = packCandidate.isFile() && packCandidate.getName().startsWith("rpmtw-1.16.zip");
-            final boolean isFolderPack = !isFilePack && packCandidate.isDirectory() && new File(packCandidate, "pack.mcmeta").isFile();
-            if (isFilePack || isFolderPack) {
-                final String packName = this.type.path + "/" + packCandidate.getName();
-                final Supplier<IResourcePack> pack = this.getAsPack(packCandidate);
-                final PackMeta meta = getPackMeta(packName, pack);
-                RpmtwUpdateMod.LOGGER.info("載入 {} {}。", this.type.displayName, packName);
-                RpmtwUpdateMod.LOGGER.debug("載入 {} 使用 {}。", packName, meta);
-                final ResourcePackInfo packInfo = ResourcePackInfo.createResourcePack(packName, meta.isBuiltIn, pack, factory, meta.priority, IPackNameDecorator.PLAIN);
-
-                if (packInfo != null) {
-                    packs.accept(packInfo);
-                }
-            } else {
-                RpmtwUpdateMod.LOGGER.error("Failed to load {} from {}. Archive packs must be zips. Folder packs must have a valid pack.mcmeta file.", this.type.displayName, packCandidate.getAbsolutePath());
-            }
-        }
-    }
-
-    public static PackMeta getPackMeta(String packName, Supplier<IResourcePack> pack) {
-        try (IResourcePack packData = pack.get()) {
-            final PackMeta meta = packData.getMetadata(META_SERIALIZER);
-            if (meta != null) {
-                return meta;
-            }
-        } catch (final IOException e) {
-            RpmtwUpdateMod.LOGGER.warn("Could not load pack meta from {}!", packName);
-            RpmtwUpdateMod.LOGGER.catching(e);
-        }
-        return new PackMeta(true, Priority.TOP, true);
-    }
-
-    private Supplier<IResourcePack> getAsPack(File file) {
-        return file.isDirectory() ? () -> new FolderPack(file) : () -> new FilePack(file);
-    }
-
-    private static File[] getFilesFromDir(File file) {
-        File[] files = new File[0];
-        if (file == null) {
-            RpmtwUpdateMod.LOGGER.error("Attempted to read from a null file.");
-        } else if (!file.isDirectory()) {
-            RpmtwUpdateMod.LOGGER.error("Can not read from {}. It's not a directory.", file.getAbsolutePath());
-        } else {
-            try {
-                final File[] readFiles = file.listFiles();
-                if (readFiles == null) {
-                    RpmtwUpdateMod.LOGGER.error("Could not read from {} due to a system error. This is likely an issue with your computer.", file.getAbsolutePath());
-                } else {
-                    files = readFiles;
-                }
-            } catch (final SecurityException e) {
-                RpmtwUpdateMod.LOGGER.error("Could not read from {}. Blocked by system level security. This is likely an issue with your computer.", file.getAbsolutePath(), e);
-            }
-        }
-        return files;
-    }
-
-    public static enum Type {
-
-        RESOURCES("PRMTW繁體中文資源包", System.getProperty("user.home") + "/.rpmtw/1.16", Configuration::allowDataPacks);
-
-        final String displayName;
-        final String path;
-        final BooleanSupplier enabled;
-
-        Type(String name, String path, BooleanSupplier enabled) {
-            this.displayName = name;
-            this.path = path;
-            this.enabled = enabled;
-        }
-    }
-
-    static class PackMetaSerializer implements IMetadataSectionSerializer<PackMeta> {
-
-        @Override
-        public String getSectionName() {
-            return "rpmtw";
-        }
-
-        @Override
-        public PackMeta deserialize(JsonObject json) {
-
-            final boolean builtin = JSONUtils.getBoolean(json, "builtin", true);
-            final String priorityName = JSONUtils.getString(json, "priority", "top").toLowerCase();
-            final Priority priority = "top".equalsIgnoreCase(priorityName) ? Priority.TOP : "bottom".equalsIgnoreCase(priorityName) ? Priority.BOTTOM : null;
-            final boolean defaultEnabled = JSONUtils.getBoolean(json, "defaultEnabled", true);
-
-            if (priority == null) {
-                throw new JsonParseException("Expected priority to be \"top\" or \"bottom\". " + priorityName + " is not a valid value!");
-            }
-            return new PackMeta(builtin, priority, defaultEnabled);
-        }
-    }
-
-    public static class PackMeta {
-        public final boolean isBuiltIn;
-        public final Priority priority;
-        public final boolean defaultEnabled;
-
-        public PackMeta(boolean isBuiltIn, Priority priority, boolean defaultEnabled) {
-            this.isBuiltIn = isBuiltIn;
-            this.priority = priority;
-            this.defaultEnabled = defaultEnabled;
-        }
-
-        @Override
-        public String toString() {
-            return "PackMeta [isBuiltIn=" + this.isBuiltIn + ", priority=" + this.priority + ", defaultEnabled=" + this.defaultEnabled + "]";
+    public void findPacks(Consumer<ResourcePackInfo> consumer, ResourcePackInfo.IFactory iFactory) {
+        ResourcePackInfo packInfo = ResourcePackInfo.createResourcePack("RPMTW-1.16.zip",
+                true, () -> new FilePack(RpmtwUpdateMod.LANGUAGE_PACK.toFile()),
+                iFactory, ResourcePackInfo.Priority.TOP, IPackNameDecorator.BUILTIN);
+        if (packInfo != null) {
+            consumer.accept(packInfo);
         }
     }
 }
--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
@@ -2,22 +2,37 @@ package siongsng.rpmtwupdatemod;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.Language;
+import net.minecraft.resources.FilePack;
+import net.minecraft.resources.IPackFinder;
+import net.minecraft.resources.IPackNameDecorator;
+import net.minecraft.resources.ResourcePackInfo;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 @Mod("rpmtw_update_mod")
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RpmtwUpdateMod {
 
     public static final Logger LOGGER = LogManager.getLogger();
+    public final static Path CACHE_DIR = Paths.get(System.getProperty("user.home") + "/.rpmtw/1.16");
+    public final static Path LANGUAGE_PACK = CACHE_DIR.resolve("RPMTW-1.16.zip");
+    public final static String LINK = json.loadJson().toString();
+    public final static long MAX_INTERVAL_DAYS = 7;
 
     public RpmtwUpdateMod() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -26,10 +41,36 @@ public class RpmtwUpdateMod {
         }
 
         if (FMLEnvironment.dist == Dist.CLIENT) {
-            Minecraft.getInstance().getResourcePackList().addPackFinder(PackFinder.RESOUCE); //載入資源包
             Minecraft.getInstance().gameSettings.language = "zh_tw"; //將語言設定為繁體中文
         }
         LOGGER.info("正在準備進行更新資源包，最新版本:" + json.ver().toString());
+
+        if (!Files.isDirectory(CACHE_DIR)) {
+            try {
+                Files.createDirectories(CACHE_DIR);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (Files.exists(LANGUAGE_PACK)) {
+            try {
+                long fileTime = Files.getLastModifiedTime(LANGUAGE_PACK).toMillis();
+                long nowTime = System.currentTimeMillis();
+                if (TimeUnit.MILLISECONDS.toDays(nowTime - fileTime) < MAX_INTERVAL_DAYS) {
+                    Minecraft.getInstance().getResourcePackList().addPackFinder(new PackFinder());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                FileUtils.copyURLToFile(new URL(LINK), LANGUAGE_PACK.toFile());
+                Minecraft.getInstance().getResourcePackList().addPackFinder(new PackFinder());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @SubscribeEvent
