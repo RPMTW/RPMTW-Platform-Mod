12281f0fc08912b18563eddc3fdce840e8df349f
菘菘 <rrt467778@gmail.com>
2021-03-26 19:07:03 +0800

    修正程式碼架構錯誤

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
@@ -3,23 +3,12 @@ package siongsng.rpmtwupdatemod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 
 @Environment(EnvType.CLIENT)
 public class RpmtwUpdateMod implements ClientModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("rpmtw_update_mod");
-    public final static Path CACHE_DIR = Paths.get(System.getProperty("user.home") + "/.rpmtw/1.16");
-    public final static Path PACK_NAME = CACHE_DIR.resolve("RPMTW-1.16.zip");
-    public final static long MAX_Hours = 1;
 
     @Override
     public void onInitializeClient() {
@@ -28,27 +17,5 @@ public class RpmtwUpdateMod implements ClientModInitializer {
         }
         LOGGER.info("Hello RPMTW world!");
         LOGGER.info("正在準備進行更新資源包，最新版本:" + json.ver().toString());
-        if (!Files.isDirectory(CACHE_DIR)) {
-            try {
-                Files.createDirectories(CACHE_DIR);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        if (Files.exists(PACK_NAME)) {
-            try {
-                long fileTime = Files.getLastModifiedTime(PACK_NAME).toMillis();
-                long nowTime = System.currentTimeMillis();
-                if (TimeUnit.MILLISECONDS.toHours(nowTime - fileTime) > MAX_Hours) {
-                    try {
-                        FileUtils.copyURLToFile(new URL(json.loadJson().toString()), PACK_NAME.toFile());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/ResourcePackManagerMixin.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/ResourcePackManagerMixin.java
@@ -3,28 +3,69 @@ package siongsng.rpmtwupdatemod.mixins;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.resource.*;
+import org.apache.commons.io.FileUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import siongsng.rpmtwupdatemod.json;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ResourcePackManager.class)
 public abstract class ResourcePackManagerMixin {
+    private final static Path CACHE_DIR = Paths.get(System.getProperty("user.home") + "/.rpmtw/1.16");
+    private final static Path PACK_NAME = CACHE_DIR.resolve("RPMTW-1.16.zip");
+    private final static long MAX_Hours = 1;
     @Shadow
     private Set<ResourcePackProvider> providers;
+
     @Inject(method = "<init>", at = @At("TAIL"))
     private void registerLoader(CallbackInfo info) {
+
         this.providers = new HashSet(this.providers);
-        try {
-            Class.forName("siongsng.rpmtwupdatemod.packs.LoadPack").getMethod("init", Set.class).invoke(null, this.providers);
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
-            e.printStackTrace();
+        if (!Files.isDirectory(CACHE_DIR)) {
+            try {
+                Files.createDirectories(CACHE_DIR);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (Files.exists(PACK_NAME)) {
+            try {
+                long fileTime = Files.getLastModifiedTime(PACK_NAME).toMillis();
+                long nowTime = System.currentTimeMillis();
+                if (TimeUnit.MILLISECONDS.toHours(nowTime - fileTime) > MAX_Hours) {
+                    try {
+                        FileUtils.copyURLToFile(new URL(json.loadJson().toString()), PACK_NAME.toFile());
+                        try {
+                            Class.forName("siongsng.rpmtwupdatemod.packs.LoadPack").getMethod("init", Set.class).invoke(null, this.providers);
+                        } catch (IllegalAccessException | ClassNotFoundException | InvocationTargetException | NoSuchMethodException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        Class.forName("siongsng.rpmtwupdatemod.packs.LoadPack").getMethod("init", Set.class).invoke(null, this.providers);
+                    } catch (IllegalAccessException | ClassNotFoundException | InvocationTargetException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
