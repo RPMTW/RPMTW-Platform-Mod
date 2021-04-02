472c79f26f5c478d5f64685004186b8364a08f46
菘菘 <rrt467778@gmail.com>
2021-04-02 16:47:20 +0800

    連動ModMenu

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/gui/ConfigScreen.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/gui/ConfigScreen.java
@@ -6,10 +6,10 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "rpmtw_update_mod")
 public class ConfigScreen implements ConfigData {
-    @Comment("crowdin = 是否啟用點擊指定快捷鍵(預設為V)後開啟翻譯網頁，並顯示相關資訊。")
+    @Comment("是否啟用點擊指定快捷鍵(預設為V)後開啟翻譯網頁，並顯示相關資訊。")
     public boolean crowdin = true;
-    @Comment("reloadpack = 是否啟用使用快捷鍵(預設為R)快速重新載入RPMTW繁體中文資源包。")
+    @Comment("是否啟用使用快捷鍵(預設為R)快速重新載入RPMTW繁體中文資源包。")
     public boolean reloadpack = true;
-    @Comment("report_translation = 是否啟用使用快捷鍵(預設為U)回報翻譯錯誤。")
+    @Comment("是否啟用使用快捷鍵(預設為U)回報翻譯錯誤。")
     public boolean report_translation = true;
 }
