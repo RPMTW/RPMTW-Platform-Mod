10f8cefc475249327fb5a9e71f0625e5a6b9323b
菘菘 <rrt467778@gmail.com>
2021-04-10 07:51:18 +0800

    新增進入世界時，是否自動發送公告的Config選項。

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/PlayJoinMixin.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/PlayJoinMixin.java
@@ -1,5 +1,6 @@
 package siongsng.rpmtwupdatemod.mixins;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -7,12 +8,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import siongsng.rpmtwupdatemod.gui.ConfigScreen;
 import siongsng.rpmtwupdatemod.notice.notice;
 
 @Mixin(PlayerManager.class)
 public class PlayJoinMixin {
+    static ConfigScreen config = AutoConfig.getConfigHolder(ConfigScreen.class).getConfig();
+
     @Inject(method = "onPlayerConnect", at = @At("TAIL"), cancellable = false)
     private void playerJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
-        notice.send(player);
+        if (config.notice) { //判斷Config
+            notice.send(player);
+        }
     }
 }
