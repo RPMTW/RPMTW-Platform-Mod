9dc4b251b844c5ae6764be21a313540c03539d77
菘菘 <rrt467778@gmail.com>
2021-04-10 09:34:45 +0800

    修復錯誤(#9)

--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -1,18 +1,21 @@
 package siongsng.rpmtwupdatemod.notice;
 
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class notice {
+    private static World world;
+
     @SubscribeEvent
     public void send(EntityJoinWorldEvent e) {
         Entity en = e.getEntity();
-        if (en == Minecraft.getInstance().player) {
+        if (en == Minecraft.getInstance().player && world != e.getWorld()) {
             en.sendMessage(new StringTextComponent(noticeGet.get().replace("\\n", "\n")), en.getUniqueID());
+            world = e.getWorld();
         }
     }
 }
