0e9cdfa10252d75b8f0acf4efa8e666036364e6c
菘菘 <rrt467778@gmail.com>
2021-04-15 23:12:01 +0800

    修復議題(#10)

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
@@ -17,12 +17,15 @@ public class RpmtwUpdateMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        AutoConfig.register(ConfigScreen.class, Toml4jConfigSerializer::new);
         key.onInitializeClient(); //註冊快捷鍵
 
         if (!ping.isConnect()) {
             LOGGER.error("你目前處於無網路狀態，因此無法使用RPMTW自動更新模組，請連結網路後重新啟動此模組。");
         }
         LOGGER.info("Hello RPMTW world!");
+    }
+
+    public RpmtwUpdateMod() {
+        AutoConfig.register(ConfigScreen.class, Toml4jConfigSerializer::new);
     }
 }
--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/function/PackVersionCheck.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/function/PackVersionCheck.java
@@ -1,8 +1,10 @@
 package siongsng.rpmtwupdatemod.function;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.resource.ResourcePackProvider;
 import org.apache.commons.io.FileUtils;
 import siongsng.rpmtwupdatemod.RpmtwUpdateMod;
+import siongsng.rpmtwupdatemod.gui.ConfigScreen;
 import siongsng.rpmtwupdatemod.json;
 
 import java.io.BufferedReader;
@@ -26,28 +28,28 @@ public class PackVersionCheck {
         RpmtwUpdateMod.LOGGER.info("正在準備檢測資源包版本，最新版本:" + Latest_ver);
         FileReader fr = new FileReader(Update_Path);
         BufferedReader br = new BufferedReader(fr);
-            int Old_ver = 0;
-            while (br.ready()) {
-                Old_ver = Integer.parseInt(br.readLine());
-                System.out.println(br.readLine());
+        int Old_ver = 0;
+        while (br.ready()) {
+            Old_ver = Integer.parseInt(br.readLine());
+            System.out.println(br.readLine());
+        }
+        fr.close();
+        try {
+            ConfigScreen config = AutoConfig.getConfigHolder(ConfigScreen.class).getConfig();
+            if (Integer.parseInt(Latest_ver_n) > Old_ver + config.Update_interval || !Files.exists(PACK_NAME)) {
+                SendMsg.send("§6偵測到資源包版本過舊，正在進行更新並重新載入中...。目前版本為:" + Old_ver + "最新版本為:" + Latest_ver_n);
+                File_Writer.Writer(Latest_ver_n, Update_Path); //寫入最新版本
+                FileUtils.copyURLToFile(new URL(json.loadJson("https://api.github.com/repos/SiongSng/ResourcePack-Mod-zh_tw/releases/latest").toString()), PACK_NAME.toFile()); //下載資源包檔案
+            } else {
+                RpmtwUpdateMod.LOGGER.info("目前的RPMTW版本已經是最新的了!!");
             }
-            fr.close();
-            try {
-                //  ConfigScreen config = AutoConfig.getConfigHolder(ConfigScreen.class).getConfig();
-                if (Integer.parseInt(Latest_ver_n) > Old_ver || !Files.exists(PACK_NAME)) {
-                    SendMsg.send("§6偵測到資源包版本過舊，正在進行更新並重新載入中...。目前版本為:" + Old_ver + "最新版本為:" + Latest_ver_n);
-                    File_Writer.Writer(Latest_ver_n, Update_Path); //寫入最新版本
-                    FileUtils.copyURLToFile(new URL(json.loadJson("https://api.github.com/repos/SiongSng/ResourcePack-Mod-zh_tw/releases/latest").toString()), PACK_NAME.toFile()); //下載資源包檔案
-                } else {
-                    RpmtwUpdateMod.LOGGER.info("目前的RPMTW版本已經是最新的了!!");
-                }
-            } catch (Exception e) {
-                RpmtwUpdateMod.LOGGER.error(e);
-            }
+        } catch (Exception e) {
+            RpmtwUpdateMod.LOGGER.error(e);
+        }
         try {
             Class.forName("siongsng.rpmtwupdatemod.packs.LoadPack").getMethod("init", Set.class).invoke(null, providers);
         } catch (Exception e) {
             RpmtwUpdateMod.LOGGER.error("發生未知錯誤" + e);
         }
-        }
+    }
 }
