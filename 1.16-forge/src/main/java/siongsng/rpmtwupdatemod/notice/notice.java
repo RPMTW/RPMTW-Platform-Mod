2cdba60b093852f9de700fe32383e9bd35a1003b
菘菘 <rrt467778@gmail.com>
2021-04-17 09:02:45 +0800

    刪除多餘Code

--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -6,8 +6,6 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class notice {
-    static String name;
-
     @SubscribeEvent
     public void send(PlayerEvent.PlayerLoggedInEvent e) {
         Entity p = e.getPlayer();
