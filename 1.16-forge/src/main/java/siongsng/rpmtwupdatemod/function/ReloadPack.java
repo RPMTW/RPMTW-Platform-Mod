a2badf5e45af67e10b75efd2855e95520e5c9512
菘菘 <rrt467778@gmail.com>
2021-04-17 22:26:20 +0800

    修復變數型別錯誤

--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/function/ReloadPack.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/function/ReloadPack.java
@@ -2,7 +2,6 @@ package siongsng.rpmtwupdatemod.function;
 
 import net.minecraft.client.Minecraft;
 import org.apache.commons.io.FileUtils;
-import siongsng.rpmtwupdatemod.RpmtwUpdateMod;
 import siongsng.rpmtwupdatemod.config.Configer;
 import siongsng.rpmtwupdatemod.json;
 
@@ -10,21 +9,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ReloadPack {
+    Path CACHE_DIR = Paths.get(System.getProperty("user.home") + "/.rpmtw/1.16");
+    Path PACK_NAME = CACHE_DIR.resolve("RPMTW-1.16.zip");
+    String Update_Path = CACHE_DIR + "/Update.txt";
+    String Latest_ver = json.ver("https://api.github.com/repos/SiongSng/ResourcePack-Mod-zh_tw/releases/latest").toString();
+    String Latest_ver_n = Latest_ver.split("RPMTW-1.16-V")[1];
+
     public ReloadPack() {
         try {
-            FileReader fr = new FileReader(RpmtwUpdateMod.Update_Path);
+            FileReader fr = new FileReader(Update_Path);
             BufferedReader br = new BufferedReader(fr);
             int Old_ver = 0;
             while (br.ready()) {
                 Old_ver = Integer.parseInt(br.readLine());
             }
             fr.close();
-            if (Integer.parseInt(RpmtwUpdateMod.Latest_ver_n) > Old_ver + Configer.Update_interval.get()) {
-                SendMsg.send("§6偵測到資源包版本過舊，正在進行更新並重新載入中...。目前版本為:" + Old_ver + "最新版本為:" + RpmtwUpdateMod.Latest_ver_n);
-                File_Writer.Writer(RpmtwUpdateMod.Latest_ver_n, RpmtwUpdateMod.Update_Path); //寫入最新版本
-                FileUtils.copyURLToFile(new URL(json.loadJson("https://api.github.com/repos/SiongSng/ResourcePack-Mod-zh_tw/releases/latest").toString()), RpmtwUpdateMod.PACK_NAME.toFile()); //下載資源包檔案
+            if (Integer.parseInt(Latest_ver_n) > Old_ver + Configer.Update_interval.get()) {
+                SendMsg.send("§6偵測到資源包版本過舊，正在進行更新並重新載入中...。目前版本為:" + Old_ver + "最新版本為:" + Latest_ver_n);
+                File_Writer.Writer(Latest_ver_n, Update_Path); //寫入最新版本
+                FileUtils.copyURLToFile(new URL(json.loadJson("https://api.github.com/repos/SiongSng/ResourcePack-Mod-zh_tw/releases/latest").toString()), PACK_NAME.toFile()); //下載資源包檔案
                 Minecraft.getInstance().reloadResources(); //重新載入資源
             } else {
                 SendMsg.send("§a目前的RPMTW版本已經是最新的了!!因此不重新載入翻譯。");
