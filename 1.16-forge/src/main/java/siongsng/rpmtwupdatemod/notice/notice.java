7b9be515dcb84315bde8720262b87b75a4386599
菘菘 <rrt467778@gmail.com>
2021-04-17 09:00:23 +0800

    修復議題(#15)和更新JDA版本

--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -1,24 +1,24 @@
 package siongsng.rpmtwupdatemod.notice;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class notice {
+    static String name;
+
     @SubscribeEvent
-    public void send(EntityJoinWorldEvent e) {
-        Entity p = e.getEntity();
-        if ((p instanceof PlayerEntity)) {
-            p.sendMessage(new StringTextComponent(
-                    "§0----------------------------------------------------\n" +
-                            "§aRPMTW自動繁化更新模組-公告\n" +
-                            "§e歡迎您使用本模組，如果遇到任何問題可以參考一下說明\n" +
-                            "§e官方Discord: https://discord.gg/5xApZtgV2u\n" +
-                            "§e查看本模組功能: https://www.rpmtw.ga/Add-on/Update-mod\n" +
-                            "§b如果有些功能不需要也可以打開設定選單關閉喔(預設快捷鍵為O)\n" +
-                            "§0----------------------------------------------------"), p.getUniqueID());
-        }
+    public void send(PlayerEvent.PlayerLoggedInEvent e) {
+        Entity p = e.getPlayer();
+        assert p != null;
+        p.sendMessage(new StringTextComponent(
+                "§0----------------------------------------------------\n" +
+                        "§aRPMTW自動繁化更新模組-公告\n" +
+                        "§e歡迎您使用本模組，如果遇到任何問題可以參考一下說明\n" +
+                        "§e官方Discord: https://discord.gg/5xApZtgV2u\n" +
+                        "§e查看本模組功能: https://www.rpmtw.ga/Add-on/Update-mod\n" +
+                        "§b如果有些功能不需要也可以打開設定選單關閉喔(預設快捷鍵為O)\n" +
+                        "§0----------------------------------------------------"), p.getUniqueID());
     }
 }
