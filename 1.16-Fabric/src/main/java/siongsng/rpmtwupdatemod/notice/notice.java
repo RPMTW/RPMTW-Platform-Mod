b1599c47a08b165e026bd52fc6ecd5d694775fbb
菘菘 <rrt467778@gmail.com>
2021-04-22 21:27:32 +0800

    模組版本檢測

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -1,11 +1,18 @@
 package siongsng.rpmtwupdatemod.notice;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
+import siongsng.rpmtwupdatemod.function.CheckChat;
 
 public class notice {
     public static void send(PlayerEntity p) {
         p.sendMessage(new TranslatableText(
                 "gui.notice"), false);
+
+        if (!new CheckChat().get()) {
+            p.sendMessage(new LiteralText(
+                    "偵測到您目前的§c §eRPMTW繁中化自動更新模組版本過舊§c 建議您更新版本，以獲得最佳體驗。"), false);
+        }
     }
 }
--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -1,9 +1,11 @@
 package siongsng.rpmtwupdatemod.notice;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import siongsng.rpmtwupdatemod.function.CheckChat;
 
 public class notice {
     @SubscribeEvent
@@ -12,5 +14,10 @@ public class notice {
         assert p != null;
         p.sendMessage(new TranslationTextComponent(
                 "gui.notice"), p.getUniqueID());
+
+        if (!new CheckChat().get()) {
+            p.sendMessage(new StringTextComponent(
+                    "偵測到您目前的§c §eRPMTW繁中化自動更新模組版本過舊§c 建議您更新版本，以獲得最佳體驗。"), p.getUniqueID());
+        }
     }
 }
