25bcce97190f9a296325bd0a46358e54e7bffaea
菘菘 <rrt467778@gmail.com>
2021-04-27 23:02:18 +0800

    修復 Fabric 首次啟動時無法載入翻譯資源包 (#23)

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/function/PackVersionCheck.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/function/PackVersionCheck.java
@@ -1,10 +1,8 @@
 package siongsng.rpmtwupdatemod.function;
 
-import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.resource.ResourcePackProvider;
 import org.apache.commons.io.FileUtils;
 import siongsng.rpmtwupdatemod.RpmtwUpdateMod;
-import siongsng.rpmtwupdatemod.config.ConfigScreen;
 import siongsng.rpmtwupdatemod.json;
 
 import java.io.BufferedReader;
@@ -25,7 +23,6 @@ public class PackVersionCheck {
             Files.createFile(Paths.get(CACHE_DIR + "/Update.txt")); //建立更新檔案
             File_Writer.Writer(Latest_ver_n, Update_Path); //寫入最新版本
         }
-        RpmtwUpdateMod.LOGGER.info("正在準備檢測資源包版本，最新版本:" + Latest_ver);
         FileReader fr = new FileReader(Update_Path);
         BufferedReader br = new BufferedReader(fr);
         int Old_ver = 0;
@@ -34,22 +31,22 @@ public class PackVersionCheck {
             System.out.println(br.readLine());
         }
         fr.close();
+        RpmtwUpdateMod.LOGGER.info("正在準備檢測資源包版本，最新版本:" + Latest_ver);
         try {
-            ConfigScreen config = AutoConfig.getConfigHolder(ConfigScreen.class).getConfig();
-            if (Integer.parseInt(Latest_ver_n) > Old_ver + config.Update_interval || !Files.exists(PACK_NAME)) {
-                SendMsg.send("§6偵測到資源包版本過舊，正在進行更新並重新載入中...。目前版本為:" + Old_ver + "最新版本為:" + Latest_ver_n);
+            if (Integer.parseInt(Latest_ver_n) > Old_ver || !Files.exists(PACK_NAME)) {
+                RpmtwUpdateMod.LOGGER.info("§6偵測到資源包版本過舊，正在進行更新並重新載入中...。目前版本為:" + Old_ver + "最新版本為:" + Latest_ver_n);
                 File_Writer.Writer(Latest_ver_n, Update_Path); //寫入最新版本
                 FileUtils.copyURLToFile(new URL(json.loadJson("https://api.github.com/repos/SiongSng/ResourcePack-Mod-zh_tw/releases/latest").toString()), PACK_NAME.toFile()); //下載資源包檔案
             } else {
                 RpmtwUpdateMod.LOGGER.info("目前的RPMTW版本已經是最新的了!!");
             }
         } catch (Exception e) {
-            RpmtwUpdateMod.LOGGER.error(e);
+            RpmtwUpdateMod.LOGGER.error("發生未知錯誤: " + e);
         }
         try {
             Class.forName("siongsng.rpmtwupdatemod.packs.LoadPack").getMethod("init", Set.class).invoke(null, providers);
         } catch (Exception e) {
-            RpmtwUpdateMod.LOGGER.error("發生未知錯誤" + e);
+            RpmtwUpdateMod.LOGGER.error("發生未知錯誤: " + e);
         }
     }
 }
