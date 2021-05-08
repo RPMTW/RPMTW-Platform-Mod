765713c650030fe522fed28fda0155be63201b91
菘菘 <rrt467778@gmail.com>
2021-05-08 22:54:18 +0800

    1.1.0-rc1

--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/crowdin/TokenCheck.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/crowdin/TokenCheck.java
@@ -26,16 +26,16 @@ public class TokenCheck {
 
         if (response.getStatusLine().getStatusCode() == 200) {
             if (Minecraft.getInstance().player != null) {
-                SendMsg.send("§9[Crowdin權杖自動檢測系統]§a檢測成功，您的Token(登入權杖)是有效的。");
+                SendMsg.send("§9[Crowdin權杖自動檢測系統]§a檢測成功，您的登入權杖是有效的。");
             }
-            RpmtwUpdateMod.LOGGER.info("[Crowdin權杖自動檢測系統]§a檢測成功，您的Token(登入權杖)是有效的。");
+            RpmtwUpdateMod.LOGGER.info("[Crowdin權杖自動檢測系統]§a檢測成功，您的登入權杖是有效的。");
             Configer.Token.set(token);
             Configer.isCheck.set(true);
         } else {
             if (Minecraft.getInstance().player != null) {
-                SendMsg.send("§9[Crowdin權杖自動檢測系統]§c檢測失敗，Token(登入權杖)無效，請再嘗試新增或至RPMTW官方Discord群組尋求協助。\n官方Discord群組:https://discord.gg/5xApZtgV2u");
+                SendMsg.send("§9[Crowdin權杖自動檢測系統]§c檢測失敗，登入權杖無效，請再嘗試新增或至RPMTW官方Discord群組尋求協助。\n官方Discord群組:https://discord.gg/5xApZtgV2u");
             }
-            RpmtwUpdateMod.LOGGER.info("[Crowdin權杖自動檢測系統]§c檢測失敗，Token(登入權杖)無效，請再嘗試新增或至RPMTW官方Discord群組尋求協助。\n官方Discord群組:https://discord.gg/5xApZtgV2u");
+            RpmtwUpdateMod.LOGGER.info("[Crowdin權杖自動檢測系統]§c檢測失敗，登入權杖無效，請再嘗試新增或至RPMTW官方Discord群組尋求協助。\n官方Discord群組:https://discord.gg/5xApZtgV2u");
             Configer.isCheck.set(false);
         }
     }
--- 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/function/CheckChat.java
+++ 1.16-forge/src/main/java/siongsng/rpmtwupdatemod/function/CheckChat.java
@@ -3,7 +3,7 @@ package siongsng.rpmtwupdatemod.function;
 import siongsng.rpmtwupdatemod.json;
 
 public class CheckChat {
-    public String ver = "1.0.9";
+    public String ver = "1.1.0";
 
     public boolean get() {
         return json.ver("https://api.github.com/repos/SiongSng/RPMTW-Update-Mod/releases/latest").toString().equals(ver);
