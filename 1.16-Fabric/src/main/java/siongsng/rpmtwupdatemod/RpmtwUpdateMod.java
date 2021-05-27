c43104944b99dc3b8a58eea1bb9342a6ccc0b058
菘菘 <rrt467778@gmail.com>
2021-05-27 13:15:33 +0800

    重新格式化程式碼並且升級Forge版本至36.1.24

--- 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
+++ 1.16-Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
@@ -29,14 +29,14 @@ public class RpmtwUpdateMod implements ClientModInitializer {
         key.onInitializeClient(); //註冊快捷鍵
         LOGGER.info("Hello RPMTW world!");
         AutoConfig.getConfigHolder(ConfigScreen.class).registerSaveListener((var1, sava) -> { //監聽儲存Config事件
-                if (!sava.Token.equals("")) {
-                    new TokenCheck().Check(sava.Token);
-                }
+            if (!sava.Token.equals("")) {
+                new TokenCheck().Check(sava.Token);
+            }
             return null;
         });
 
         if (!Configer.config.Token.equals("")) { //如果Token不是空的
-                new TokenCheck().Check(Configer.config.Token); //開始檢測
+            new TokenCheck().Check(Configer.config.Token); //開始檢測
         }
 
     }
