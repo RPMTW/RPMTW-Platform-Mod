a0c4dc45000914024c62e5690dddfb4093200235
菘菘 <rrt467778@gmail.com>
2021-06-13 20:50:43 +0800

    修復某些人的電腦進入世界時會崩潰問題

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -1,9 +1,9 @@
 package siongsng.rpmtwupdatemod.notice;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import siongsng.rpmtwupdatemod.function.CheckModVersion;
-import siongsng.rpmtwupdatemod.function.SendMsg;
 
 public class notice {
     public static void send(PlayerEntity p) {
@@ -11,7 +11,7 @@ public class notice {
                 "gui.notice"), false);
 
         if (!new CheckModVersion().get()) {
-            SendMsg.send("偵測到您目前的§c §eRPMTW繁中化自動更新模組版本過舊§c\n建議您更新版本，以獲得最佳體驗。\n目前版本: " + new CheckModVersion().ver + " 最新版本: " + new CheckModVersion().NewVer() + "\n下載連結:https://bit.ly/33MpXu8");
+            p.sendMessage(new LiteralText("偵測到您目前的§c §eRPMTW繁中化自動更新模組版本過舊§c\n建議您更新版本，以獲得最佳體驗。\n目前版本: " + new CheckModVersion().ver + " 最新版本: " + new CheckModVersion().NewVer() + "\n下載連結:https://bit.ly/33MpXu8"), false);
         }
     }
 }
