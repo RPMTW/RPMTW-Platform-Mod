0b67a9988637f059c826183b0673a1c594a06d62
菘菘 <rrt467778@gmail.com>
2021-06-15 14:44:41 +0800

    Delete some code

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/function/ReloadPack.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/function/ReloadPack.java
@@ -5,7 +5,6 @@ import net.minecraft.client.gui.screen.SplashScreen;
 import net.minecraft.client.resource.ResourceReloadLogger;
 import net.minecraft.resource.ProfiledResourceReload;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Util;
 import org.apache.commons.io.FileUtils;
@@ -18,26 +17,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class ReloadPack {
     final static Path PackDir = Paths.get(System.getProperty("user.home") + "/.rpmtw/1.17");
     final static Path PackFile = PackDir.resolve("RPMTW-1.17.zip");
 
     public ReloadPack() {
-        SendMsg.send("由於你按下了檢測翻譯包更新快捷鍵，因此開始進行翻譯包更新檢測作業，請稍後...");
+        SendMsg.send("由於你按下了翻譯包更新快捷鍵，正在執行更新中，請稍後...");
         Thread thread = new Thread(() -> {
             if (Files.exists(PackFile)) {
                 try {
-                    long fileTime = Files.getLastModifiedTime(PackFile).toMillis();
-                    long nowTime = System.currentTimeMillis();
-                    if (TimeUnit.MILLISECONDS.toMinutes(nowTime - fileTime) < 1) {
-                        SendMsg.send("§6偵測到翻譯包版本過舊，正在進行更新並重新載入中...。");
                         FileUtils.copyURLToFile(new URL(RpmtwUpdateMod.PackDownloadUrl), PackFile.toFile()); //下載資源包檔案
                         reloadLanguage();
-                    } else {
-                        SendMsg.send("§a目前的RPMTW翻譯包版本已經是最新的了!因此不進行更新作業。");
-                    }
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
