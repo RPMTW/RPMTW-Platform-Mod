de91bb94a588e31a679f5abdfe66a68d9b0d6ab7
菘菘 <rrt467778@gmail.com>
2021-07-17 21:16:08 +0800

    新增變更日誌

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/function/CheckModVersion.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/function/CheckModVersion.java
@@ -12,6 +12,9 @@ public class CheckModVersion {
     public String NewVer() {
         return json.get("https://raw.githubusercontent.com/RPMTW/ResourcePack-Mod-zh_tw/main/ModUpdate.json").getAsJsonObject("Fabric").getAsJsonObject("1.17").getAsJsonPrimitive("latest").getAsString();
     }
+    public String ChangeLog() {
+        return json.get("https://raw.githubusercontent.com/RPMTW/ResourcePack-Mod-zh_tw/main/ModUpdate.json").getAsJsonObject("Fabric").getAsJsonObject("1.17").getAsJsonPrimitive("ChangeLog").getAsString();
+    }
 
     public String DownloadUrl() {
         return json.get("https://raw.githubusercontent.com/RPMTW/ResourcePack-Mod-zh_tw/main/ModUpdate.json").getAsJsonObject("Fabric").getAsJsonObject("1.17").getAsJsonPrimitive("DownloadUrl").getAsString();
--- Fabric/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/notice/notice.java
@@ -11,7 +11,10 @@ public class notice {
                 "gui.notice"), false);
 
         if (!new CheckModVersion().get()) {
-            p.sendMessage(new LiteralText("偵測到您目前的§c §eRPMTW繁中化自動更新模組版本過舊§c\n建議您更新版本，以獲得最佳體驗。\n目前版本: " + new CheckModVersion().ver + " 最新版本: " + new CheckModVersion().NewVer() + "\n下載連結:https://www.rpmtw.ga"), false);
+            p.sendMessage(new LiteralText("偵測到您目前的§c §eRPMTW繁中化自動更新模組版本過舊§c\n建議您更新版本，以獲得最佳體驗。\n目前版本: "
+                    + new CheckModVersion().ver + " 最新版本: "
+                    + new CheckModVersion().NewVer() + "\n下載連結:https://www.rpmtw.ga\n變更日誌: "
+                    + new CheckModVersion().ChangeLog()), false);
         }
     }
 }
