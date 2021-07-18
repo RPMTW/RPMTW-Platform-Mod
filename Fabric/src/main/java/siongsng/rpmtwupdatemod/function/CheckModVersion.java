2e407c385f3754a96e8c26862a341556d6e3d8c0
菘菘 <rrt467778@gmail.com>
2021-07-18 23:34:11 +0800

    修復1.17.0崩潰問題 (1.2.1)

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/function/CheckModVersion.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/function/CheckModVersion.java
@@ -3,7 +3,7 @@ package siongsng.rpmtwupdatemod.function;
 import siongsng.rpmtwupdatemod.json;
 
 public class CheckModVersion {
-    public String ver = "1.2.0";
+    public String ver = "1.2.1";
 
     public boolean get() {
         return json.get("https://raw.githubusercontent.com/RPMTW/ResourcePack-Mod-zh_tw/main/ModUpdate.json").getAsJsonObject("Fabric").getAsJsonObject("1.17").getAsJsonPrimitive("latest").getAsString().equals(ver);
