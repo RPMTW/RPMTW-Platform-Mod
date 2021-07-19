913803615ef282e8a519ed6e610c5e646d0909e9
菘菘 <rrt467778@gmail.com>
2021-07-19 18:00:08 +0800

    init

--- Forge/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
+++ Forge/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
@@ -1,14 +1,9 @@
 package siongsng.rpmtwupdatemod;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(
         modid = RpmtwUpdateMod.MOD_ID,
@@ -17,9 +12,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 )
 public class RpmtwUpdateMod {
 
-    public static final String MOD_ID = "RPMTW-Update-Mod";
+    public static final String MOD_ID = "rpmtw_update_mod";
     public static final String MOD_NAME = "RPMTW Update Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2.1";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
@@ -51,63 +46,4 @@ public class RpmtwUpdateMod {
     public void postinit(FMLPostInitializationEvent event) {
 
     }
-
-    /**
-     * Forge will automatically look up and bind blocks to the fields in this class
-     * based on their registry name.
-     */
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Blocks {
-      /*
-          public static final MySpecialBlock mySpecialBlock = null; // placeholder for special block below
-      */
-    }
-
-    /**
-     * Forge will automatically look up and bind items to the fields in this class
-     * based on their registry name.
-     */
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Items {
-      /*
-          public static final ItemBlock mySpecialBlock = null; // itemblock for the block above
-          public static final MySpecialItem mySpecialItem = null; // placeholder for special item below
-      */
-    }
-
-    /**
-     * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
-     */
-    @Mod.EventBusSubscriber
-    public static class ObjectRegistryHandler {
-        /**
-         * Listen for the register event for creating custom items
-         */
-        @SubscribeEvent
-        public static void addItems(RegistryEvent.Register<Item> event) {
-           /*
-             event.getRegistry().register(new ItemBlock(Blocks.myBlock).setRegistryName(MOD_ID, "myBlock"));
-             event.getRegistry().register(new MySpecialItem().setRegistryName(MOD_ID, "mySpecialItem"));
-            */
-        }
-
-        /**
-         * Listen for the register event for creating custom blocks
-         */
-        @SubscribeEvent
-        public static void addBlocks(RegistryEvent.Register<Block> event) {
-           /*
-             event.getRegistry().register(new MySpecialBlock().setRegistryName(MOD_ID, "mySpecialBlock"));
-            */
-        }
-    }
-    /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
-    public static class MySpecialItem extends Item {
-
-    }
-
-    public static class MySpecialBlock extends Block {
-
-    }
-    */
 }
