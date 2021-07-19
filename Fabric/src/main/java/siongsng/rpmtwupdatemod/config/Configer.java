ab942d1ba9d66ca3a0e5686b2727678aae7fb8d8
菘菘 <rrt467778@gmail.com>
2021-07-19 17:39:10 +0800

    Delete Fabric directory

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/config/Configer.java
+++
@@ -1,7 +0,0 @@
-package siongsng.rpmtwupdatemod.config;
-
-import me.shedaniel.autoconfig.AutoConfig;
-
-public class Configer {
-    public static ConfigScreen config = AutoConfig.getConfigHolder(ConfigScreen.class).getConfig();
-}
