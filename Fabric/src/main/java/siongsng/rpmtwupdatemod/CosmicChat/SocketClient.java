70d227dc214476ac04bce8a299abf3acb74e68e2
菘菘 <rrt467778@gmail.com>
2021-10-10 14:59:50 +0800

    暫時先不新增新驗證機制，修改一些內容

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/CosmicChat/SocketClient.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/CosmicChat/SocketClient.java
@@ -1,13 +1,7 @@
 package siongsng.rpmtwupdatemod.CosmicChat;
 
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import customUtils._class35312;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import net.minecraft.client.MinecraftClient;
@@ -18,11 +12,14 @@ import siongsng.rpmtwupdatemod.RpmtwUpdateMod;
 import siongsng.rpmtwupdatemod.config.RPMTWConfig;
 import siongsng.rpmtwupdatemod.function.SendMsg;
 
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SocketClient {
     private static Socket socket;
 
-    public static void init()
-    {
+    public static void init() {
         try {
             MinecraftClient mc = MinecraftClient.getInstance();
             Session session = mc.getSession();
@@ -30,79 +27,80 @@ public class SocketClient {
             Map<String, String> AuthMap = new HashMap<>();
             String Token = session.getAccessToken();
             String UUID = session.getUuid();
-            String cs = _class35312.M(Token+UUID);
-           
-            AuthMap.put("Token", session.getAccessToken());
-            AuthMap.put("UUID", session.getUuid());
-            AuthMap.put("CS",cs);
-            
+            AuthMap.put("Token", Token);
+            AuthMap.put("UUID", UUID);
+
             IO.Options options = IO.Options.builder().setAuth(AuthMap).build();
-            socket = IO.socket(_class35312.AU("x2"), options).connect();
+            socket = IO.socket("https://api.rpmtwchat.ga", options).connect();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static void GetMessage() {
-    	if(socket == null)
-    		init();
-    	
-    	socket.on(("broadcast"), (data) -> {
-           try {
-               if (MinecraftClient.getInstance() == null) return;
-               if (!RPMTWConfig.getConfig().isChat) return;
+        if (socket == null) {
+            init();
+        }
 
-               MinecraftClient mc = MinecraftClient.getInstance();
-               PlayerEntity player = mc.player;
-               Session session = mc.getSession();
-               assert player != null;
+        socket.on(("broadcast"), (data) -> {
+            try {
+                if (MinecraftClient.getInstance() == null)
+                    return;
+                if (!RPMTWConfig.getConfig().isChat)
+                    return;
 
-               JsonParser jp = new JsonParser();
-               JsonObject JsonData = (JsonObject) jp.parse(data[0].toString());
-               String Type = JsonData.getAsJsonPrimitive("Type").getAsString();
-               String MessageType = JsonData.getAsJsonPrimitive("MessageType").getAsString();
+                MinecraftClient mc = MinecraftClient.getInstance();
+                PlayerEntity player = mc.player;
+                Session session = mc.getSession();
+                assert player != null;
 
-               switch (Type) {
-                   case "Client":
-                       if (MessageType.equals("General")) { //一般類型的訊息
-                           String UserName = JsonData.getAsJsonPrimitive("UserName").getAsString();
-                           String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
-                           if (UserName.equals("菘菘#8663") || UserName.equals("SiongSng")) {
-                               UserName = "§bRPMTW維護者";
-                           }
-                           SendMsg.send(String.format(("§9[宇宙通訊] §e<§6%s§e> §f%s"), UserName, Message));
-                       }
-                   case "Server":
-                       switch (MessageType) {
-                           case "Ban" -> {
-                               String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
-                               if (session.getUuid().equals(UUID)) {
-                                   SendMsg.send("由於您違反了 《RPMTW 宇宙通訊系統終端使用者授權合約》，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
-                               }
-                           }
-                           case "Auth" -> {
-                               String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
-                               if (session.getUuid().equals(UUID)) {
-                                   SendMsg.send("由於您的 Minecraft 帳號不是正版，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
-                               }
-                           }
-                           case "Notice" -> {
-                               String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
-                               SendMsg.send(String.format(("§c[官方公告] §f%s"), Message));
-                           }
-                       }
-               }
+                JsonParser jp = new JsonParser();
+                JsonObject JsonData = (JsonObject) jp.parse(data[0].toString());
+                String Type = JsonData.getAsJsonPrimitive("Type").getAsString();
+                String MessageType = JsonData.getAsJsonPrimitive("MessageType").getAsString();
 
-           } catch (Exception err) {
-               RpmtwUpdateMod.LOGGER.warn("接收宇宙通訊訊息時發生未知錯誤，原因: " + err);
-           }
-       });
+                switch (Type) {
+                    case "Client":
+                        if (MessageType.equals("General")) { // 一般類型的訊息
+                            String UserName = JsonData.getAsJsonPrimitive("UserName").getAsString();
+                            String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
+                            if (UserName.equals("菘菘#8663") || UserName.equals("SiongSng")) {
+                                UserName = "§bRPMTW維護者";
+                            }
+                            SendMsg.send(String.format(("§9[宇宙通訊] §e<§6%s§e> §f%s"), UserName, Message));
+                        }
+                    case "Server":
+                        switch (MessageType) {
+                            case "Ban" -> {
+                                String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
+                                if (session.getUuid().equals(UUID)) {
+                                    SendMsg.send("由於您違反了 《RPMTW 宇宙通訊系統終端使用者授權合約》，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
+                                }
+                            }
+                            case "Auth" -> {
+                                String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
+                                if (session.getUuid().equals(UUID)) {
+                                    SendMsg.send("由於您的 Minecraft 帳號不是正版，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
+                                }
+                            }
+                            case "Notice" -> {
+                                String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
+                                SendMsg.send(String.format(("§c[官方公告] §f%s"), Message));
+                            }
+                        }
+                }
 
-   }
-    
+            } catch (Exception err) {
+                RpmtwUpdateMod.LOGGER.warn("接收宇宙通訊訊息時發生未知錯誤，原因: " + err);
+            }
+        });
+
+    }
+
     public static void Send(String Message) {
-    	if(socket == null)
-    		init();
+        if (socket == null) {
+            init();
+        }
         try {
             MinecraftClient mc = MinecraftClient.getInstance();
             PlayerEntity player = mc.player;
@@ -121,20 +119,20 @@ public class SocketClient {
             RpmtwUpdateMod.LOGGER.warn("發送宇宙通訊訊息時發生未知錯誤，原因: " + err);
         }
     }
-    
+
     public Socket getSocket() {
-    	if(socket == null)
-    		init();
-    	
+        if (socket == null)
+            init();
+
         return socket;
     }
-    
+
     public static void Disconnect() {
-    	if(socket != null)
-    		socket.disconnect();
-    	
-    	socket = null;
-    	RpmtwUpdateMod.LOGGER.info("已中斷宇宙通訊的連線");
+        if (socket != null)
+            socket.disconnect();
+
+        socket = null;
+        RpmtwUpdateMod.LOGGER.info("已中斷宇宙通訊的連線");
     }
-    
+
 }
--- Fabric/src/main/java/siongsng/rpmtwupdatemod/gui/EULA.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/gui/EULA.java
@@ -22,12 +22,13 @@ public class EULA extends LightweightGuiDescription {
         WGridPanel gui = new WGridPanel();
         setRootPanel(gui);
         gui.setSize(300, 200);
-        WLabel label = new WLabel(new LiteralText("宇宙通訊系統-EULA"), 0xFF5555);
+        WLabel label = new WLabel(new LiteralText("《RPMTW 宇宙通訊系統終端使用者授權合約》"), 0xFF5555);
         gui.add(label, (int) 6.8, 1, 2, 1);
-        gui.add(new WLabel(new LiteralText("使用宇宙通訊系統須遵守《RPMTW 宇宙通訊系統終端使用者授權合約》"), 0xFFFFFF), 1, 3, 2, 1);
-        gui.add(new WLabel(new LiteralText("- 由於此功能與Discord串聯，請遵守《Discord使用者服務條款》"), 0xFFFFFF), 1, 4, 2, 1);
-        gui.add(new WLabel(new LiteralText("- 訊息內容請不得以任何形式騷擾別人，也禁止使用任何攻擊手段攻擊宇宙通訊伺服器，否則我們有權封禁該帳號"), 0xFFFFFF), 1, 5, 2, 1);
-        gui.add(new WLabel(new LiteralText("- 我們將會蒐集您的 Minecraft UUID/ID 與 登入憑證，憑證與UUID僅用於驗證與封禁帳號"), 0xFFFFFF), 1, 6, 2, 1);
+        gui.add(new WLabel(new LiteralText("如需使用宇宙通訊系統須遵守以下條款，違者我們有權封禁該帳號"), 0xFFFFFF), 1, 3, 2, 1);
+
+        gui.add(new WLabel(new LiteralText("- 此功能與Discord串聯，請遵守《Discord使用者服務條款》"), 0xFFFFFF), 1, 4, 2, 1);
+        gui.add(new WLabel(new LiteralText("- 訊息不得以任何形式騷擾別人，也禁止攻擊宇宙通訊伺服器"), 0xFFFFFF), 1, 5, 2, 1);
+        gui.add(new WLabel(new LiteralText("- 我們將會蒐集您的 Minecraft 憑證，僅用於驗證與封禁帳號"), 0xFFFFFF), 1, 6, 2, 1);
         gui.add(new WLabel(new LiteralText("- 我們將有權隨時更改本條款"), 0xFFFFFF), 1, 7, 2, 1);
 
         WButton Cancel = new WButton(new LiteralText("我不同意"));
