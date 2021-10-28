8e5c83792b993633b4a5e87349c78d982605b3d7
菘菘 <rrt467778@gmail.com>
2021-10-28 21:06:32 +0800

    修復公告訊息換行錯誤

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/PlayJoinMixin.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/PlayJoinMixin.java
@@ -16,7 +16,7 @@ public class PlayJoinMixin {
     private void playerJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
         if (RPMTWConfig.getConfig().notice) { //判斷Config
             player.sendMessage(new LiteralText(
-                    "§a歡迎使用 RPMTW Update Mod 萬用中文化模組\\n§f如有問題可以到我們的Discord群組詢問\\n§fDiscord: https://discord.gg/5xApZtgV2u\\n§f如果有些功能不需要也可以打開選單關閉(快捷鍵 O)"), false);
+                    "§a歡迎使用 RPMTW Update Mod 萬用中文化模組\n§f如有問題可以到我們的Discord群組詢問\n§fDiscord: https://discord.gg/5xApZtgV2u\n§f如果有些功能不需要也可以打開選單關閉(快捷鍵 O)"), false);
         }
     }
 }
--- Forge/src/main/java/siongsng/rpmtwupdatemod/function/onPlayerJoin.java
+++ Forge/src/main/java/siongsng/rpmtwupdatemod/function/onPlayerJoin.java
@@ -11,6 +11,6 @@ public class onPlayerJoin {
         Entity p = e.getPlayer();
         assert p != null;
         p.sendMessage(new TextComponent(
-                "§a歡迎使用 RPMTW Update Mod 萬用中文化模組\\n§f如有問題可以到我們的Discord群組詢問\\n§fDiscord: https://discord.gg/5xApZtgV2u\\n§f如果有些功能不需要也可以打開選單關閉(快捷鍵 O)"), p.getUUID());
+                "§a歡迎使用 RPMTW Update Mod 萬用中文化模組\n§f如有問題可以到我們的Discord群組詢問\n§fDiscord: https://discord.gg/5xApZtgV2u\n§f如果有些功能不需要也可以打開選單關閉(快捷鍵 O)"), p.getUUID());
     }
 }
