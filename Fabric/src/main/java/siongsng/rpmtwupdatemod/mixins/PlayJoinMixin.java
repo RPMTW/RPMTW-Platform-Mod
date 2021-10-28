e26c5b2558f571e501ea1cbca50fa0b51303039b
菘菘 <rrt467778@gmail.com>
2021-10-28 20:46:30 +0800

    修改進入世界公告訊息內容

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/PlayJoinMixin.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/mixins/PlayJoinMixin.java
@@ -12,11 +12,11 @@ import siongsng.rpmtwupdatemod.config.Configer;
 
 @Mixin(PlayerManager.class)
 public class PlayJoinMixin {
-    @Inject(method = "onPlayerConnect", at = @At("TAIL"), cancellable = false)
+    @Inject(method = "onPlayerConnect", at = @At("TAIL"))
     private void playerJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
         if (Configer.getConfig().notice) { //判斷Config
             player.sendMessage(new LiteralText(
-                    "§a歡迎使用 RPMTW Update Mod 萬用中文化模組\n§9如有問題可以到我們的Discord群組詢問\n§9Discord: https://discord.gg/5xApZtgV2u\n§9如果有些功能不需要也可以打開選單關閉(快捷鍵 O)"), false);
+                    "§a歡迎使用 RPMTW Update Mod 萬用中文化模組\n§f如有問題可以到我們的Discord群組詢問\n§fDiscord: https://discord.gg/5xApZtgV2u\n§f如果有些功能不需要也可以打開選單關閉(快捷鍵 O)"), false);
         }
     }
 }
