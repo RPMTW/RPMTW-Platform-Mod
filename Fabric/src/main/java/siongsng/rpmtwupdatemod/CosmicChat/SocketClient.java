b82052ce23cc07582508f58bc53784664050edce
SiongSng <rrt467778@gmail.com>
2021-12-25 12:16:27 +0800

    改善貢獻者按鈕放置布局

--- Fabric/src/main/java/siongsng/rpmtwupdatemod/CosmicChat/SocketClient.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/CosmicChat/SocketClient.java
@@ -51,43 +51,45 @@ public class SocketClient {
 
                 MinecraftClient mc = MinecraftClient.getInstance();
                 PlayerEntity player = mc.player;
-                Session session = mc.getSession();
-                assert player != null;
 
-                JsonParser jp = new JsonParser();
-                JsonObject JsonData = (JsonObject) jp.parse(data[0].toString());
-                String Type = JsonData.getAsJsonPrimitive("Type").getAsString();
-                String MessageType = JsonData.getAsJsonPrimitive("MessageType").getAsString();
+                if (player != null) {
+                    Session session = mc.getSession();
 
-                switch (Type) {
-                    case "Client":
-                        if (MessageType.equals("General")) { // 一般類型的訊息
-                            String UserName = JsonData.getAsJsonPrimitive("UserName").getAsString();
-                            String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
-                            if (UserName.equals("菘菘#8663") || UserName.equals("SiongSng")) {
-                                UserName = "§bRPMTW維護者";
-                            }
-                            SendMsg.send(String.format(("§9[宇宙通訊] §e<§6%s§e> §f%s"), UserName, Message));
-                        }
-                    case "Server":
-                        switch (MessageType) {
-                            case "Ban" -> {
-                                String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
-                                if (session.getUuid().equals(UUID)) {
-                                    SendMsg.send("由於您違反了 《RPMTW 宇宙通訊系統終端使用者授權合約》，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
-                                }
-                            }
-                            case "Auth" -> {
-                                String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
-                                if (session.getUuid().equals(UUID)) {
-                                    SendMsg.send("由於您的 Minecraft 帳號不是正版，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
-                                }
-                            }
-                            case "Notice" -> {
+                    JsonParser jp = new JsonParser();
+                    JsonObject JsonData = (JsonObject) jp.parse(data[0].toString());
+                    String Type = JsonData.getAsJsonPrimitive("Type").getAsString();
+                    String MessageType = JsonData.getAsJsonPrimitive("MessageType").getAsString();
+
+                    switch (Type) {
+                        case "Client":
+                            if (MessageType.equals("General")) { // 一般類型的訊息
+                                String UserName = JsonData.getAsJsonPrimitive("UserName").getAsString();
                                 String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
-                                SendMsg.send(String.format(("§c[官方公告] §f%s"), Message));
+                                if (UserName.equals("菘菘#8663") || UserName.equals("SiongSng")) {
+                                    UserName = "§bRPMTW維護者";
+                                }
+                                SendMsg.send(String.format(("§9[宇宙通訊] §e<§6%s§e> §f%s"), UserName, Message));
                             }
-                        }
+                        case "Server":
+                            switch (MessageType) {
+                                case "Ban" -> {
+                                    String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
+                                    if (session.getUuid().equals(UUID)) {
+                                        SendMsg.send("由於您違反了 《RPMTW 宇宙通訊系統終端使用者授權合約》，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
+                                    }
+                                }
+                                case "Auth" -> {
+                                    String UUID = JsonData.getAsJsonPrimitive("UUID").getAsString();
+                                    if (session.getUuid().equals(UUID)) {
+                                        SendMsg.send("由於您的 Minecraft 帳號不是正版，因此無法發送訊息至宇宙通訊，如認為有誤判請至我們的Discord群組。");
+                                    }
+                                }
+                                case "Notice" -> {
+                                    String Message = JsonData.getAsJsonPrimitive("Message").getAsString();
+                                    SendMsg.send(String.format(("§c[官方公告] §f%s"), Message));
+                                }
+                            }
+                    }
                 }
 
             } catch (Exception err) {
--- Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
+++ Fabric/src/main/java/siongsng/rpmtwupdatemod/RpmtwUpdateMod.java
@@ -3,6 +3,23 @@ package siongsng.rpmtwupdatemod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import siongsng.rpmtwupdatemod.CosmicChat.SocketClient;
@@ -13,8 +30,11 @@ import siongsng.rpmtwupdatemod.crowdin.TokenCheck;
 import siongsng.rpmtwupdatemod.gui.ping;
 import siongsng.rpmtwupdatemod.translation.Handler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 @Environment(EnvType.CLIENT)
 public class RpmtwUpdateMod implements ClientModInitializer {
@@ -37,12 +57,42 @@ public class RpmtwUpdateMod implements ClientModInitializer {
         new RPMKeyBinding().Register(); //註冊快捷鍵
         LOGGER.info("Hello RPMTW world!");
         if (!RPMTWConfig.getConfig().Token.equals("")) { //如果Token不是空的
-            new TokenCheck().Check(RPMTWConfig.getConfig().Token); //開始檢測
+            new TokenCheck().Check(RPMTWConfig.getConfig().Token); //開始檢測 Crowdin token
         }
         if (RPMTWConfig.getConfig().isChat) {
             SocketClient.GetMessage();
         }
         new Handler();
+        ScreenEvents.AFTER_INIT.register(this::screenEvent);
+    }
+
+    public void screenEvent(MinecraftClient client, Screen screen, int scaledWidth, int scaledHeight) {
+        boolean show = screen instanceof GameMenuScreen || screen instanceof CreativeInventoryScreen || screen instanceof InventoryScreen || screen instanceof OptionsScreen;
+
+        if (show && RPMTWConfig.getConfig().contributorButton && RPMTWConfig.getConfig().isChinese) {
+
+
+            TexturedButtonWidget rpmtwLogo = new TexturedButtonWidget(scaledWidth / 80, scaledHeight - 22, 15, 15, 0, 0, 0, new Identifier(RpmtwUpdateMod.Mod_ID, "textures/rpmtw_logo.png"), 15, 15, (buttonWidget) -> {
+            }, new LiteralText(""));
+            ButtonWidget buttonWidget = new ButtonWidget(scaledWidth / 80 - 2, scaledHeight - 25, 20, 20, LiteralText.EMPTY, (button) -> Util.getOperatingSystem().open("https://www.rpmtw.com/Contributor"), new ButtonWidget.TooltipSupplier() {
+                public void onTooltip(ButtonWidget buttonWidget, MatrixStack matrixStack, int i, int j) {
+                    this.supply(consumer -> {
+                        List<OrderedText> lines = new ArrayList<>();
+                        lines.add(consumer.asOrderedText());
+                        screen.renderOrderedTooltip(matrixStack, lines
+                                , i, j);
+                    });
+                }
+
+                public void supply(Consumer<Text> consumer) {
+                    consumer.accept(new LiteralText("查看 RPMTW 翻譯貢獻者"));
+                }
+            });
+
+            List<ClickableWidget> buttons = Screens.getButtons(screen);
+            buttons.add(buttonWidget);
+            buttons.add(rpmtwLogo);
+        }
     }
 
 }
