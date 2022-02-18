010557f12ef029fe6336f66eb3ef5c973143d066
SiongSng <rrt467778@gmail.com>
2022-02-18 18:05:14 +0800

    example test

--- forge/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/forge/RPMTWPlatformModForge.kt
+++ forge/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/forge/RPMTWPlatformModForge.kt
@@ -3,20 +3,67 @@ package com.rpmtw.rpmtw_platform_mod.forge
 import com.rpmtw.rpmtw_platform_mod.RPMTWPlatformMod
 import com.rpmtw.rpmtw_platform_mod.RPMTWPlatformMod.init
 import dev.architectury.platform.forge.EventBuses
+import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_CONTEXT
+import thedarkcolour.kotlinforforge.forge.runForDist
 
 @Mod(RPMTWPlatformMod.MOD_ID)
-class RPMTWPlatformModForge {
+object RPMTWPlatformModForge {
+//    init {
+//        EventBuses.registerModEventBus(RPMTWPlatformMod.MOD_ID, MOD_CONTEXT.getKEventBus())
+//        MOD_BUS.addListener(this::init)
+//    }
+//
+//    private fun init(event: FMLCommonSetupEvent) {
+//        init()
+//    }
+
+
+    const val ID = "examplemod"
+
+    // the logger for our mod
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
     init {
-        EventBuses.registerModEventBus(RPMTWPlatformMod.MOD_ID, MOD_CONTEXT.getKEventBus())
-        MOD_BUS.addListener(this::init)
+        LOGGER.log(Level.INFO, "Hello world!")
+
+        // Register the KDeferredRegister to the mod-specific event bus
+        ModBlocks.REGISTRY.register(MOD_BUS)
+
+        val obj = runForDist(
+            clientTarget = {
+                MOD_BUS.addListener(::onClientSetup)
+                Minecraft.getInstance()
+            },
+            serverTarget = {
+                MOD_BUS.addListener(::onServerSetup)
+                "test"
+            })
+
+        println(obj)
     }
 
-    private fun init(event: FMLCommonSetupEvent) {
-        init()
+    /**
+     * This is used for initializing client specific
+     * things such as renderers and keymaps
+     * Fired on the mod specific event bus.
+     */
+    private fun onClientSetup(event: FMLClientSetupEvent) {
+        LOGGER.log(Level.INFO, "Initializing client...")
     }
 
+    /**
+     * Fired on the global Forge bus.
+     */
+    private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
+        LOGGER.log(Level.INFO, "Server starting...")
+    }
 }
