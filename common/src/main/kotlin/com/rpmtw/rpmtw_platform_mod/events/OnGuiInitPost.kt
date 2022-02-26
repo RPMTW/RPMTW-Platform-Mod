66e354ff8e54eb88e0e7a08af95aa55f118a09e8
SiongSng <rrt467778@gmail.com>
2022-02-26 21:10:38 +0800

    feat: quark

--- common/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/events/OnGuiInitPost.kt
+++ common/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/events/OnGuiInitPost.kt
@@ -6,6 +6,7 @@ import com.rpmtw.rpmtw_platform_mod.mixins.ChatScreenAccessor
 import com.rpmtw.rpmtw_platform_mod.utilities.RPMTWConfig
 import com.rpmtw.rpmtw_platform_mod.utilities.Utilities
 import dev.architectury.hooks.client.screen.ScreenAccess
+import dev.architectury.platform.Platform
 import net.minecraft.client.gui.components.EditBox
 import net.minecraft.client.gui.screens.ChatScreen
 import net.minecraft.client.gui.screens.Screen
@@ -19,9 +20,17 @@ object OnGuiInitPost {
 
         if (screen is ChatScreen && (RPMTWConfig.get().cosmicChat.enable && RPMTWConfig.get().cosmicChat.enableButton)) {
             val textField: EditBox? = (screen as ChatScreenAccessor.chatFieldAccessor).chatField
+            val offsetX: Int
+            val hasQuarkMod = Platform.isModLoaded("quark")
+            offsetX = if (hasQuarkMod) {
+                // Because quark mod has a button on the chat screen, we need to offset the button by the width of the quark button
+                // https://github.com/VazkiiMods/Quark/blob/9c3334244d508d0b0383e7f397b02c136295067e/src/main/java/vazkii/quark/content/tweaks/module/EmotesModule.java#L224
+                -75
+            } else {
+                0
+            }
 
-            val sendButton = TranslucentButton(
-                scaledWidth - 185,
+            val sendButton = TranslucentButton(scaledWidth - 185 + offsetX,
                 scaledHeight - 40,
                 90,
                 20,
@@ -31,16 +40,12 @@ object OnGuiInitPost {
                 },
                 { _, matrixStack, i, j ->
                     screen.renderTooltip(
-                        matrixStack,
-                        TranslatableComponent("cosmicChat.rpmtw_platform_mod.button.send.tooltip"),
-                        i,
-                        j
+                        matrixStack, TranslatableComponent("cosmicChat.rpmtw_platform_mod.button.send.tooltip"), i, j
                     )
-                }
-            )
+                })
 
             val checkbox = RPMTWCheckbox(
-                scaledWidth - 90,
+                scaledWidth - 90 + offsetX,
                 scaledHeight - 40,
                 20,
                 20,
--- common/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/handlers/EventHandler.kt
+++ common/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/handlers/EventHandler.kt
@@ -1,5 +1,6 @@
 package com.rpmtw.rpmtw_platform_mod.handlers
 
+import com.rpmtw.rpmtw_platform_mod.RPMTWPlatformMod
 import com.rpmtw.rpmtw_platform_mod.events.OnClientStarted
 import com.rpmtw.rpmtw_platform_mod.events.OnClientStopping
 import com.rpmtw.rpmtw_platform_mod.events.OnGuiInitPost
@@ -10,6 +11,10 @@ object EventHandler {
     fun handle() {
         ClientLifecycleEvent.CLIENT_STOPPING.register(::OnClientStopping)
         ClientLifecycleEvent.CLIENT_STARTED.register(::OnClientStarted)
-        ClientGuiEvent.INIT_POST.register(OnGuiInitPost::handle)
+        try {
+            ClientGuiEvent.INIT_POST.register(OnGuiInitPost::handle)
+        } catch (e: Exception) {
+            RPMTWPlatformMod.LOGGER.error("Adding button to the chat screen failed", e)
+        }
     }
 }
