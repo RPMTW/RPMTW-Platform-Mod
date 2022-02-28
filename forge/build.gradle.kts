b6bf54368f9c4681034119c4cb24e3ae008ae760
SiongSng <rrt467778@gmail.com>
2022-02-28 17:12:25 +0800

    fix: build jar can't run in-game

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -27,11 +27,19 @@ dependencies {
     modApi("dev.architectury:architectury-forge:${project.property("architectury_version").toString()}")
     modApi("me.shedaniel.cloth:cloth-config-forge:${project.property("cloth_config_version").toString()}")
     implementation("thedarkcolour:kotlinforforge:${project.property("kotlin_forge_version").toString()}")
-    forgeRuntimeLibrary(
+
+    "shadowCommon"(
         "com.github.RPMTW:RPMTW-API-Client-Kotlin:${
             project.property("rpmtw_api_client_version").toString()
         }"
-    )
+    ) {
+        exclude("com.google.code.gson")
+        exclude("org.jetbrains.kotlinx")
+        exclude("org.jetbrains.kotlin")
+    }.let {
+        forgeRuntimeLibrary(it)
+    }
+
     forgeRuntimeLibrary(kotlin("stdlib-jdk8"))
     forgeRuntimeLibrary(kotlin("reflect"))
 
