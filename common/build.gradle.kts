0e5480a4b3ae3ffa0b1ccc0267f726fb3207d0ac
菘菘 <rrt467778@gmail.com>
2022-08-09 14:49:35 +0800

    refactor: shadow jar (#167)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     // Remove the next line if you don't want to depend on the API
     modApi("dev.architectury:architectury:${project.property("architectury_version").toString()}")
     modApi("me.shedaniel.cloth:cloth-config:${project.property("cloth_config_version").toString()}")
-    modApi("com.github.RPMTW:RPMTW-API-Client-Kotlin:${project.property("rpmtw_api_client_version").toString()}")
+    modApi("com.github.RPMTW:RPMTW-API-Client-Kotlin:${project.property("rpmtw_api_client_version")}")
 }
 
 architectury {
@@ -14,17 +14,4 @@ architectury {
 
 loom {
     accessWidenerPath.set(file("src/main/resources/rpmtw_platform_mod.accesswidener"))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenCommon") {
-            artifactId = project.property("archives_base_name").toString()
-            from(components["java"])
-        }
-    }
-
-    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
-    repositories {
-    }
 }
