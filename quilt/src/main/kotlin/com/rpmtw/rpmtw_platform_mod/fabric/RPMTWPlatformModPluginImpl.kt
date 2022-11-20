6ba08115a726ed67e502fa7daca32794f9b9e0ab
BennyDioxide <bennystyang@gmail.com>
2022-11-20 12:23:41 +0800

    feat: replace registerClientCommand with dispatchClientCommand due to merge

--- quilt/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/fabric/RPMTWPlatformModPluginImpl.kt
+++ quilt/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/fabric/RPMTWPlatformModPluginImpl.kt
@@ -1,30 +1,13 @@
 package com.rpmtw.rpmtw_platform_mod.fabric
 
 import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.arguments.ArgumentType
-import com.mojang.brigadier.context.CommandContext
-
 import net.minecraft.commands.CommandBuildContext
-import net.minecraft.commands.Commands
-import net.minecraft.resources.ResourceLocation
-import net.minecraft.server.packs.PackType
-import net.minecraft.server.packs.resources.PreparableReloadListener
-import net.minecraft.server.packs.resources.ResourceManager
-import net.minecraft.server.packs.resources.SimplePreparableReloadListener
-import net.minecraft.util.profiling.ProfilerFiller
-
+import net.minecraft.commands.SharedSuggestionProvider
 import org.quiltmc.loader.api.QuiltLoader
-import org.quiltmc.qsl.command.api.client.ClientCommandManager
 import org.quiltmc.qsl.command.api.client.ClientCommandRegistrationCallback
 import org.quiltmc.qsl.command.api.client.QuiltClientCommandSource
-import org.quiltmc.qsl.resource.loader.api.ResourceLoader
-import org.quiltmc.qsl.resource.loader.api.reloader.IdentifiableResourceReloader
-
-import com.rpmtw.rpmtw_platform_mod.RPMTWPlatformMod
-
+import org.quiltmc.qsl.command.impl.client.ClientCommandInternals
 import java.io.File
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Executor
 
 @Suppress("unused")
 object RPMTWPlatformModPluginImpl {
@@ -34,67 +17,20 @@ object RPMTWPlatformModPluginImpl {
     }
 
     @JvmStatic
-    fun registerClientCommand(
-        command: String,
-        subCommand: String,
-        argumentName: String? = null,
-        argumentType: ArgumentType<*>? = null,
-        executes: (CommandContext<*>) -> Int
-    ) {
-        ClientCommandRegistrationCallback.EVENT.register(ClientCommandRegistrationCallback {
-                dispatcher: CommandDispatcher<QuiltClientCommandSource>,
-                _: CommandBuildContext?,
-                _: Commands.CommandSelection ->
-            if (argumentName != null && argumentType != null) {
-                dispatcher.register(
-                    ClientCommandManager.literal(command).then(
-                        ClientCommandManager.literal(subCommand)
-                            .then(ClientCommandManager.argument(argumentName, argumentType).executes {
-                                return@executes executes(it)
-                            })
-                    )
-                )
-            } else {
-                dispatcher.register(ClientCommandManager.literal(command).then(ClientCommandManager.literal(subCommand).executes {
-                    return@executes executes(it)
-                }))
-            }
+    fun dispatchClientCommand(callback: (CommandDispatcher<SharedSuggestionProvider>, buildContext: CommandBuildContext) -> Unit) {
+        ClientCommandRegistrationCallback.EVENT.register(ClientCommandRegistrationCallback { dispatcher: CommandDispatcher<QuiltClientCommandSource?>, buildContext: CommandBuildContext, _ ->
+            @Suppress("UNCHECKED_CAST")
+            callback(dispatcher as CommandDispatcher<SharedSuggestionProvider>, buildContext)
         })
     }
 
     @JvmStatic
-    fun <T> registerReloadEvent(reloadListener: SimplePreparableReloadListener<T>) {
-        ResourceLoader.get(PackType.CLIENT_RESOURCES)
-            .registerReloader(PrepareableReloadListenerWrapper(reloadListener))
+    fun executeClientCommand(command: String): Boolean {
+        return ClientCommandInternals.executeCommand(command, false)
     }
 
     @JvmStatic
     fun getGameFolder(): File {
         return QuiltLoader.getGameDir().toFile()
-    }
-}
-
-@Suppress("SpellCheckingInspection")
-private class PrepareableReloadListenerWrapper<T>(val reloadListener: SimplePreparableReloadListener<T>) :
-    IdentifiableResourceReloader {
-    override fun reload(
-        preparationBarrier: PreparableReloadListener.PreparationBarrier,
-        resourceManager: ResourceManager,
-        profilerFiller: ProfilerFiller,
-        profilerFiller2: ProfilerFiller,
-        executor: Executor,
-        executor2: Executor
-    ): CompletableFuture<Void> {
-        return reloadListener.reload(
-            preparationBarrier,
-            resourceManager,
-            profilerFiller,
-            profilerFiller2,
-            executor,
-            executor2)
-    }
-
-    override fun getQuiltId(): ResourceLocation {
-        return ResourceLocation("${RPMTWPlatformMod.MOD_ID}/reload_listener_${reloadListener.hashCode()}")
     }
 }
