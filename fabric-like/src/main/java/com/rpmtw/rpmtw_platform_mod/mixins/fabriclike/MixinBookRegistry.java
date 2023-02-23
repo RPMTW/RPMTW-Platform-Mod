b711ff79037a6c2471da2ad529d7219247d92833
SiongSng <rrt467778@gmail.com>
2023-02-23 18:51:13 +0800

    fix: translations for Patchouli books

--- fabric-like/src/main/java/com/rpmtw/rpmtw_platform_mod/mixins/fabriclike/MixinBookRegistry.java
+++ fabric-like/src/main/java/com/rpmtw/rpmtw_platform_mod/mixins/fabriclike/MixinBookRegistry.java
@@ -1,39 +1,39 @@
-///*
-//Credit: https://github.com/kappa-maintainer/PRP
-// */
-//package com.rpmtw.rpmtw_platform_mod.mixins.fabriclike;
-//
-//import net.minecraft.resources.ResourceLocation;
-//import org.spongepowered.asm.mixin.Final;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.Shadow;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//import vazkii.patchouli.client.book.ClientBookRegistry;
-//import vazkii.patchouli.common.base.PatchouliConfig;
-//import vazkii.patchouli.common.book.Book;
-//import vazkii.patchouli.common.book.BookRegistry;
-//
-//import java.util.Map;
-//
-//@Mixin(value = BookRegistry.class, remap = false)
-//public class MixinBookRegistry {
-//    @Shadow
-//    @Final
-//    public Map<ResourceLocation, Book> books;
-//
-//    @Shadow
-//    private boolean loaded;
-//
-//    @Inject(method = "reloadContents", at = @At("HEAD"), cancellable = true)
-//    public void reloadContents(boolean resourcePackBooksOnly, CallbackInfo ci) {
-//        PatchouliConfig.reloadBuiltinFlags();
-//        for (Book book : books.values()) {
-//            book.reloadContents(false);
-//        }
-//        ClientBookRegistry.INSTANCE.reloadLocks(false);
-//        loaded = true;
-//        ci.cancel();
-//    }
-//}
+/*
+Credit: https://github.com/kappa-maintainer/PRP
+ */
+package com.rpmtw.rpmtw_platform_mod.mixins.fabriclike;
+
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vazkii.patchouli.client.book.ClientBookRegistry;
+import vazkii.patchouli.common.base.PatchouliConfig;
+import vazkii.patchouli.common.book.Book;
+import vazkii.patchouli.common.book.BookRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(value = BookRegistry.class, remap = false)
+public class MixinBookRegistry {
+    @Final
+    @Shadow
+    public final Map<ResourceLocation, Book> books = new HashMap<>();
+    @Shadow
+    private boolean loaded = false;
+
+    @Inject(method = "reloadContents", at = @At("HEAD"), cancellable = true)
+    public void reloadContents(boolean resourcePackBooksOnly, CallbackInfo ci) {
+        PatchouliConfig.reloadBuiltinFlags();
+        for (Book book : books.values()) {
+            book.reloadContents(false);
+        }
+        ClientBookRegistry.INSTANCE.reloadLocks(false);
+        loaded = true;
+        ci.cancel();
+    }
+}
