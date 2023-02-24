785df29991447a961428b58ff0c8076eff3a6a35
SiongSng <rrt467778@gmail.com>
2023-02-24 23:10:57 +0800

    fix: better message

--- common/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/events/OnPlayerJoin.kt
+++ common/src/main/kotlin/com/rpmtw/rpmtw_platform_mod/events/OnPlayerJoin.kt
@@ -5,22 +5,30 @@ import dev.architectury.event.events.common.PlayerEvent
 import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.HoverEvent
 import net.minecraft.server.level.ServerPlayer
 
 class OnPlayerJoin : PlayerEvent.PlayerJoin {
     override fun join(player: ServerPlayer) {
         if (!RPMTWConfig.get().firstJoinLevel) return
 
-        val officailWebsiteLink = Component.literal("rpmtw.com").withStyle {
-            it.withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, "https://rpmtw.com")).withColor(ChatFormatting.BLUE)
+        val officailWebsiteLink = Component.literal("https://rpmtw.com").withStyle {
+            it.withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, "https://rpmtw.com"))
+                .withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("gui.rpmtw_platform_mod_open_link")))
+                .withColor(ChatFormatting.BLUE)
         }
-        val officialDiscordLink = Component.literal("discord.gg/5xApZtgV2u").withStyle {
-            it.withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/5xApZtgV2u")).withColor(ChatFormatting.BLUE)
+        val officialDiscordLink = Component.literal("https://discord.gg/5xApZtgV2u").withStyle {
+            it.withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/5xApZtgV2u"))
+                .withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("gui.rpmtw_platform_mod_open_link")))
+                .withColor(ChatFormatting.BLUE)
         }
-        val message = Component.translatable("messages.rpmtw_platform_mod.thanks", officailWebsiteLink, officialDiscordLink)
+        val settingsKey = RPMTWConfig.get().keyBindings.config.localizedName.copy().withStyle {
+            it.withColor(ChatFormatting.RED)
+        }
+
+        val message = Component.translatable("messages.rpmtw_platform_mod.thanks", officailWebsiteLink, officialDiscordLink, settingsKey)
 
         player.sendSystemMessage(message)
-
-        // RPMTWConfig.get().firstJoinLevel = false
+        RPMTWConfig.get().firstJoinLevel = false
     }
 }
